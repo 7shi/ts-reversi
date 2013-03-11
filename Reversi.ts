e785e9e16118abe337d603fe444cc670d7216db2
7shi <7shi@live.jp>
2013-03-11 21:12:05 +0900

    クラス化

--- Reversi.ts
+++ Reversi.ts
@@ -4,38 +4,41 @@ canvas.height = 260
 document.body.appendChild(canvas)
 var ctx = canvas.getContext("2d")
 
-var board = [
-	[0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 2, 1, 0, 0, 0],
-	[0, 0, 0, 1, 2, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0],
-]
-
-drawBoard()
-
-function drawBoard() {
-	ctx.fillStyle = "green"
-	ctx.fillRect(0, 0, canvas.width, canvas.height)
-	ctx.strokeStyle = "black"
-	for (var i = 0; i <= 8; i++)
-	{
-		ctx.beginPath()
-		ctx.moveTo(i * 30 + 10,  10)
-		ctx.lineTo(i * 30 + 10, 250)
-		ctx.moveTo( 10, i * 30 + 10)
-		ctx.lineTo(250, i * 30 + 10)
-		ctx.stroke()
-	}
-	for (var y = 0; y <= 7; y++) {
-		for (var x = 0; x <= 7; x++) {
-			drawStone(x, y, board[y][x])
+class Board {
+	board : number[] = [
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 2, 1, 0, 0, 0],
+		[0, 0, 0, 1, 2, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0],
+	];
+	
+	draw() {
+		ctx.fillStyle = "green"
+		ctx.fillRect(0, 0, canvas.width, canvas.height)
+		ctx.strokeStyle = "black"
+		for (var i = 0; i <= 8; i++)
+		{
+			ctx.beginPath()
+			ctx.moveTo(i * 30 + 10,  10)
+			ctx.lineTo(i * 30 + 10, 250)
+			ctx.moveTo( 10, i * 30 + 10)
+			ctx.lineTo(250, i * 30 + 10)
+			ctx.stroke()
+		}
+		for (var y = 0; y <= 7; y++) {
+			for (var x = 0; x <= 7; x++) {
+				drawStone(x, y, this.board[y][x])
+			}
 		}
 	}
 }
+
+var board = new Board
+board.draw()
 
 function drawStone(x: number, y: number, c: number) {
 	if (c == 1) {
